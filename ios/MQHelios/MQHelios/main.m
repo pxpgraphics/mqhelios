ad7022e95c43382103d6f992bc90da8fdd3e1b4d
Paris Xavier Pinkney <paris@pxpgraphics.com>
2014-06-23 14:19:24 -0700

    Added MQHelios project.

---
+++ ios/MQHelios/MQHelios/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MQHelios
+//
+//  Created by Paris Pinkney on 6/23/14.
+//  Copyright (c) 2014 Marqeta, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
